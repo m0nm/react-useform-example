ac0be048cdb65eb5a9b6d4c37432649f923bb0ff
monm <mounimnotakatsuki@gmail.com>
2022-03-11 15:14:57 +0100

    final code

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,73 @@
 import "./App.css";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors },
+  } = useForm();
   return (
     <div className="container">
-      <form>
+      <form
+        onSubmit={handleSubmit(() =>
+          console.log("user registered successfully!")
+        )}
+      >
         <h1>Sign Up</h1>
         {/* email */}
         <div className="input-container">
+          <p className="error-message">{errors.email?.message}</p>
           <label htmlFor="email">Email</label>
-          <input type="email" name="email" id="email" />
+          <input
+            {...register("email", {
+              required: "Please Enter Your Email!",
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: "Please Enter A Valid Email!",
+              },
+            })}
+            type="email"
+            id="email"
+            className={`${errors.email && "input-error"}`}
+          />
         </div>
 
         {/* password */}
         <div className="input-container">
+          <p className="error-message">{errors.password?.message}</p>
           <label htmlFor="password">password</label>
-          <input type="password" id="password" />
+          <input
+            {...register("password", {
+              required: "Please Enter Your Password",
+              minLength: {
+                value: 8,
+                message: "Password must be at least 8 characters long!",
+              },
+            })}
+            type="password"
+            id="password"
+            className={`${errors.password && "input-error"}`}
+          />
         </div>
 
         {/* confirm password */}
         <div className="input-container">
+          <p className="error-message">{errors.confirmPassword?.message}</p>
           <label htmlFor="confirmPassword">Confirm Password</label>
-          <input type="password" id="confirmPassword" />
+          <input
+            {...register("confirmPassword", {
+              required: "Please Confirm Your Password",
+              validate: (match) => {
+                const password = getValues("password");
+                return match === password || "Passwords should match!";
+              },
+            })}
+            type="password"
+            id="confirmPassword"
+            className={`${errors.confirmPassword && "input-error"}`}
+          />
         </div>
 
         {/* button */}
